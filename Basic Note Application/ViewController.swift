0b3bf705999045668bb120f328add4f02b04ac64
ibrahimkultepe <ibrahimkultepe@gmail.com>
2022-09-16 22:47:56 +0300

    .

--- Basic Note Application/ViewController.swift
+++ Basic Note Application/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let viewModel: ViewModel
+    
+    init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
 
---
+++ Basic Note Application/ViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewModel.swift
+//  Basic Note Application
+//
+//  Created by İbrahim Kültepe on 16.09.2022.
+//
+
+import Foundation
+
+class ViewModel {
+    
+}
