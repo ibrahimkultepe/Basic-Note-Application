bfd5559bf634960fc1cb592e3c109020879ec2e1
ibrahimkultepe <ibrahimkultepe@gmail.com>
2022-09-16 22:27:37 +0300

    Initial Commit

---
+++ Basic Note Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic Note Application
+//
+//  Created by İbrahim Kültepe on 16.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
